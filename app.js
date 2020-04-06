b5d4700dca39e3adfd8db9c26149c8f3c435cea7
jaden0747 <jaden0747@gmail.com>
2020-04-06 20:47:02 +0700

    add callbacks to btns

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, 
+- In each turn, a player rolls a dice as many times as he whishes. Each result get 
+added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's 
+the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added 
+to his GLOBAL score. After that, 
 it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
@@ -14,45 +17,97 @@ it's the next player's turn
 // ─── INITIALIZE ─────────────────────────────────────────────────────────────────
 //
 
-// eslint-disable-next-line no-unused-vars
 var scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-
-document.querySelector(".dice").style.display = "none";
+initNewGame();
 
 //
 // ─────────────────────────────────────────────────────────── END INITIALIZE ─────
 //
 
 //
+// ─── FUNCITON ───────────────────────────────────────────────────────────────────
+//
+
+function changeActivePlayer() {
+     document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+     activePlayer = 1 - activePlayer;
+     document.querySelector(".player-" + activePlayer + "-panel").classList.add("active");
+}
+
+function updateScore() {
+     // 1. update score
+     document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer] + roundScore;
+
+     // 2. reset player's current score
+     roundScore = 0;
+     document.querySelector("#current-" + activePlayer).textContent = 0;
+}
+
+function initNewGame() {
+     document.querySelector(".dice").style.display = "none";
+     document.getElementById("score-0").textContent = "0";
+     document.getElementById("score-1").textContent = "0";
+     document.getElementById("current-0").textContent = "0";
+     document.getElementById("current-1").textContent = "0";
+}
+//
+// ───────────────────────────────────────────────────────────── END FUNCTION ─────
+//
+
+//
 // ─── CALLBACK ───────────────────────────────────────────────────────────────────
 //
 
-document.querySelector(".btn-roll").addEventListener("click", function () {
+document.querySelector(".btn-roll").addEventListener("click", function() {
      // 1. generate a random number
      var dice = Math.floor(Math.random() * 6) + 1;
      var current = document.querySelector("#current-" + activePlayer);
-     var image = document.querySelector(".dice");
+     var diceDOM = document.querySelector(".dice");
 
      // 2. display result image
      current.textContent = dice + 1;
-     image.display = "block";
-     image.src = "/asset/dice-" + dice + ".png";
+     diceDOM.style.display = "block";
+     diceDOM.src = "/asset/dice-" + dice + ".png";
+
      // 3. update round score if the number != 1
      if (dice !== 1) {
+          // add score
           roundScore += dice;
           current.textContent = roundScore;
      }
+
      // 4. if the number == 1, discard all the score earned and switch player's turn
      else {
-          // reset round score
+          // update player's score
           roundScore = 0;
-          current.textContent = roundScore;
-          // switch player's turn
-          activePlayer = 1 - activePlayer;
+          updateScore();
+
+          // change active player
+          changeActivePlayer();
      }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+     // 1. Update player's score
+     scores[activePlayer] += roundScore;
+     roundScore = 0; // reset round score
+     updateScore();
+
+     // 2. change active player
+     changeActivePlayer();
+});
+
+document.querySelector(".btn-new").addEventListener("click", function() {
+     // 1. reset document content
+     initNewGame();
+
+     // 2. reset global variables
+     scores = [0, 0];
+     roundScore = 0;
+     activePlayer = 0;
 });
 //
 // ───────────────────────────────────────────────────────────────── CALLBACK ─────
